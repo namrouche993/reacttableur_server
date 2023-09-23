de35bcd54de5d49b2e6cf242ba58e15a1bf8058a
namrouche993 <namrouche993@gmail.com>
2023-09-24 00:46:27 +0100

    im with CSRF attack , see App14 in react_remaind and express_test

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ const secretKey = '425cac990d726cd10669e2957c6f2ebef6e2b1f4f61dffc011c7327e73031
 
 var bodyParser = require('body-parser')
 const requestIp = require('request-ip');
+const generateRandomString = require('./Randst_server.js');
 
 const app = express()
 
 app.use(express.json());
-app.use(cors({credentials: true}));
+app.use(cors({origin: 'http://localhost:3000',credentials: true}));  // Use the cors middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
 
@@ -56,11 +57,39 @@ app.post('/register',authenticate, (req, res) => {
  });
 
 
+ app.post('/beacondata',express.json(), express.text(), (req, res) => {
+  // Handle the received data
+  const receivedData = JSON.parse(req.body);
+  console.log(receivedData)
+  console.log(receivedData[7][1])
+
+  // Process the received data as needed !
+  //console.log('Received data:', receivedData);
+
+  // Respond with a success message
+  res.status(200).send('Data received successfully.');
+});
+
  app.get('/profile',authenticate, (req, res) => {
    res.send(`Welcome, ${req.user.email}! This is your profile.`);
  });
 
- 
+
+app.post('/save-data',(req,res)=>{
+  const { datar_received } = req.body ;
+  console.log('data received succesfully ! ')
+  console.log(datar_received)
+  res.status(201).send('Data received successfully');
+});
+
+app.get('/api/firstlogin',(req,res)=>{
+  console.log('we are inside /api/firstlog ')
+  const idusername_first = generateRandomString(14);
+  console.log(idusername_first);
+  const token_first = jwt.sign({ idusername_first }, secretKey);
+  res.cookie('jwtToken_first', token_first, { httpOnly: true,  maxAge: 8640000000 });
+  res.json({ idusername_first });
+})
 
 app.post('/api/login', (req, res) => {
   console.log('we will call api/login nnnnnnnnnnnnnnnnnnnnnnn')
@@ -78,18 +107,18 @@ app.post('/api/login', (req, res) => {
   const token = jwt.sign({ idusername }, secretKey);
   console.log(token)
   res.cookie('jwtToken', token, { httpOnly: true,  maxAge: 8640000000 });
-  res.cookie('cookie_name', 'cookie_value');  
+  //res.cookie('cookie_name', 'cookie_value');  
   res.json({ token });
 });
 
 
 const authorizeUser = (req, res, next) => {
   console.log('authorizeUser');
-  const token = req.header('Authorization');
+  //const token = req.header('Authorization');
+  //console.log(token);
+  
+  const token = req.cookies.jwtToken;
   console.log(token);
-  const jwtTokenget = req.cookies.jwtToken;
-  console.log(req.cookies);  
-  console.log(jwtTokenget);
 
   const idusername = req.params.idusername;
   console.log(idusername)
@@ -101,8 +130,8 @@ const authorizeUser = (req, res, next) => {
 
   try {
     console.log('we are inside try')
-    console.log(token.split([' '])[1])
-    const decoded = jwt.verify(token.split([' '])[1], secretKey);
+    console.log(token)
+    const decoded = jwt.verify(token, secretKey);
     console.log('decoded :')
     console.log(decoded)
     if (decoded.idusername !== idusername) {
@@ -117,7 +146,7 @@ const authorizeUser = (req, res, next) => {
 };
 
 
-app.get("/api/:idusername", authorizeUser , async (req,res)=>{
+app.get("/api2/:idusername", authorizeUser , async (req,res)=>{
   try {
     console.log('we are in api:username')
     const idusername = req.params.idusername;
