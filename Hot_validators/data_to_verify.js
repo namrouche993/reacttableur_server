ec0ee7bd262a8b601e2306caa3aeb9b254a7b1ef
namrouche993 <namrouche993@gmail.com>
2023-09-26 16:48:24 +0100

    data verfify

--- Hot_validators/data_to_verify.js
+++ Hot_validators/data_to_verify.js
@@ -5,7 +5,7 @@
   ['',"Ministére de l'Habitat, de l'Urbanisme et de la Ville",'','','','','','','','','','','','','','','',   ''],
   ['',"Etat d'éxecution des travaux de raccordement en énergies électrique et gaziéres  - Projets achevés ou dépassants les 50% d'avancement",'','','','','','','','','','','','','','','',   ''],
 
-  [null,'Organisme :','','','','','','','','','','','','','','','',''],
+  ['','Organisme :','','','','','','','','','','','','','','','',''],
 
 
   ['','Projet','Consistance de Projet',"Taux d'\navancement\ndes travaux\nlogements","Taux d'avancement des travaux VRD",'Raccordement en énergie Electrique','','','','','Raccordement en énergie Gaziere','','','','',   ''],
@@ -27,15 +27,15 @@ const last_row_after_header = 15 // editable must be equal to the one in initial
 function ddatafct(last_row_after_header){
     var ddata= [];
     ddata.push(  
-      ['empty',"start0with_République Algérienne Démocratique et Populaire",'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',   'empty'],
-      ['empty',"start0with_Ministére de l'Habitat, de l'Urbanisme et de la Ville",'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',   'empty'],
-      ['empty',"start0with_Etat d'éxecution des travaux de raccordement en énergies électrique et gaziéres  - Projets achevés ou dépassants les 50% d'avancement",'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',   'empty'],
+      ['empty',"sta_rt_0_abc_with_République Algérienne Démocratique et Populaire",'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',   'empty'],
+      ['empty',"sta_rt_0_abc_with_Ministére de l'Habitat, de l'Urbanisme et de la Ville",'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',   'empty'],
+      ['empty',"sta_rt_0_abc_with_Etat d'éxecution des travaux de raccordement en énergies électrique et gaziéres  - Projets achevés ou dépassants les 50% d'avancement",'empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty',   'empty'],
 
       ['empty','modal0from_Organisme :','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty','empty'],
 
 
-      ['empty','start0with_Projet','start0with_Consistance de Projet',"start0with_Taux d'\navancement\ndes travaux\nlogements","start0with_Taux d'avancement des travaux VRD",'start0with_Raccordement en énergie Electrique','empty','empty','empty','empty','start0with_Raccordement en énergie Gaziere','empty','empty','empty','empty',   'empty'],
-      ['empty','empty','empty','empty','empty',"start0with_Taux d'avancement des travaux",'start0with_Montant des travaux (devis SADEG)','start0with_Montant payé (DA)','start0with_Créances détenues travaux','start0with_Contraintes',"start0with_Taux d'avancement des travaux",'start0with_Montant des travaux (devis SADEG)','start0with_Montant payé (DA)','start0with_Créances détenues travaux','start0with_Contraintes',   'empty'],
+      ['empty','sta_rt_0_abc_with_Projet','sta_rt_0_abc_with_Consistance de Projet',"sta_rt_0_abc_with_Taux d'\navancement\ndes travaux\nlogements","sta_rt_0_abc_with_Taux d'avancement des travaux VRD",'sta_rt_0_abc_with_Raccordement en énergie Electrique','empty','empty','empty','empty','sta_rt_0_abc_with_Raccordement en énergie Gaziere','empty','empty','empty','empty',   'empty'],
+      ['empty','empty','empty','empty','empty',"sta_rt_0_abc_with_Taux d'avancement des travaux",'sta_rt_0_abc_with_Montant des travaux (devis SADEG)','sta_rt_0_abc_with_Montant payé (DA)','sta_rt_0_abc_with_Créances détenues travaux','sta_rt_0_abc_with_Contraintes',"sta_rt_0_abc_with_Taux d'avancement des travaux",'sta_rt_0_abc_with_Montant des travaux (devis SADEG)','sta_rt_0_abc_with_Montant payé (DA)','sta_rt_0_abc_with_Créances détenues travaux','sta_rt_0_abc_with_Contraintes',   'empty'],
     )
     
     if( retreived_data[0].length !== ddata[0].length ) {
@@ -45,35 +45,51 @@ function ddatafct(last_row_after_header){
     if (retreived_data.length < ddata.length ) {
         return false;
     }
-    console.log('we depasse row length')
-    if( retreived_data.every(row => row[0] == '' || row[0] == null )
-     && retreived_data.every(row => row[ Number(retreived_data[0].length)-1 ] == '' || row[ Number(retreived_data[0].length)-1 ] == null )
-    ) {
-        //console.log('0 col and last col are equal to "" or null ')
-    } else {
-        console.log('0 and last col are not all equal to "" or NULLL !!!')
-        return false;
-    }
-    console.log('we depasse 0 col and last col that are equal')
 
-    let AreReallyEmpty = true;
-    const ddata_empty_index = findIndicesOfTargetValue(ddata,'empty');
-    for (let index = 0; index < ddata_empty_index.length; index++) {
-      if( 
-        retreived_data[Number(ddata_empty_index[index].rowIndex)][Number(ddata_empty_index[index].colIndex)]=='' ||
-        retreived_data[Number(ddata_empty_index[index].rowIndex)][Number(ddata_empty_index[index].colIndex)]==null 
-      ) {
-      } else {
-        AreReallyEmpty=false;
+  let AreReallySomething = true;
+  for (let i = 0; i < retreived_data.length; i++) {
+    for (let j = 0; j < retreived_data[0].length; j++) {
+      console.log("i is : " + i + " j is : " + j);
+      console.log(ddata[i][j])
+      console.log(retreived_data[i][j])
+
+      if( j==0 && !(retreived_data[i][j]=='' || retreived_data[i][j]==null ) ) {
+        console.log('we depasse 1');
+        AreReallySomething=false;
         break;
-      }
-      //const element = ddata_empty_index[index];
+       }
+       else if( j== Number(Number(retreived_data[0].length)-1) && !(retreived_data[i][j]=='' || retreived_data[i][j]==null ) ) {
+        AreReallySomething=false;
+        break;
+       }
+       else if(ddata[i][j]=='empty' && !( retreived_data[i][j]=='' || retreived_data[i][j]==null ) ){
+        console.log('we depasse 2');         
+        AreReallySomething=false;
+        break;
+        }
+       else if( ddata[i][j].includes('sta_rt_0_abc_with_') && retreived_data[i][j]!==ddata[i][j].replace('sta_rt_0_abc_with_','') ) {
+        console.log('we depasse 3');         
+        AreReallySomething=false;
+          break;
+        }
+      console.log('we depasse 4');       
+
     }
-    if(!AreReallyEmpty){
-      return false
+   if(!AreReallySomething){
+       break;
+   }
+
+  }
+  //console.log(i)
+  //console.log(j)
+  console.log(AreReallySomething)
+
+  if(!AreReallySomething){
+    return false
   }
 
-    console.log('we depasse empty equal to "" ')
+
+  console.log('we depasse empty equal to "" ')
 
 
   /*  
